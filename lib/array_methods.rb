43b252cad266055eb684531f54d7a838ad010dea
Leon Jamison <leonjamison@icloud.com>
2020-01-03 04:49:51 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -14,13 +14,13 @@ def find_max_value(array = [1,2,1,3,4,3,5,4,3,2,1])
   array.length.times { |index| x = array[index] if array[index] > x }
   x
 end
-
-def find_min_value(array)
-  x = array[0]
-  array.length.times do |index|
-    if array[index] < x
-      x = array[index]
-    end
-  end
-  x
-end
+#
+# def find_min_value(array)
+#   x = array[0]
+#   array.length.times do |index|
+#     if array[index] < x
+#       x = array[index]
+#     end
+#   end
+#   x
+# end
